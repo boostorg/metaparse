ff0ddb2edb9fc8d778e86089b496b7754f31a4ba
Abel Sinkovics <abelsoft@freemail.hu>
2015-07-09 09:27:20 +0200

    Static linking against Boost.Test

--- test/string.cpp
+++ test/string.cpp
@@ -3,7 +3,6 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE string
 
 #define BOOST_LIMIT_STRING_SIZE 64
